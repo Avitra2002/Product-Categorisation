0fab6ea7e43484c64758570fb349b4d5cc8a2054
AngieAvitraPhon <avitra_phon@mymail.sutd.edu.sg>
2024-08-11 14:36:27 +0800

    Integration done

--- older_version/Functions/social_media.py
+++ older_version/Functions/social_media.py
@@ -4,9 +4,9 @@
 
 
 
-from Functions.is_english import is_english
-from Functions.format_date import format_date
-from Functions.valid_feedback import is_valid_feedback
+from Data_processing_cloud.Functions.is_english import is_english
+from Data_processing_cloud.Functions.format_date import format_date
+from Data_processing_cloud.Functions.valid_feedback import is_valid_feedback
 import pandas as pd
 from langdetect import detect
 from langdetect.lang_detect_exception import LangDetectException
@@ -14,17 +14,18 @@ from datetime import datetime
 import logging
 import google.cloud.logging
 from google.cloud.logging.handlers import CloudLoggingHandler
-from Classifications.Classifications_products import classification_defined_products
-from Classifications.Classification_Others import classification_undefined_products
+from Data_processing_cloud.Classifications.Classifications_products import classification_defined_products
+from Data_processing_cloud.Classifications.Classification_Others import classification_undefined_products
 import logging
-from pubsub_helper import publish_message
+from Data_processing_cloud.pubsub_helper import publish_message
+from Data_processing_cloud.matching_subproduct import find_best_match
 
 # Initialize Cloud Logging
-client = google.cloud.logging.Client()
-handler = CloudLoggingHandler(client)
-cloud_logger = logging.getLogger('cloudLogger')
-cloud_logger.setLevel(logging.DEBUG)
-cloud_logger.addHandler(handler)
+# client = google.cloud.logging.Client()
+# handler = CloudLoggingHandler(client)
+# cloud_logger = logging.getLogger('cloudLogger')
+# cloud_logger.setLevel(logging.DEBUG)
+# cloud_logger.addHandler(handler)
 
 #### FOR LOCAL TEST######
 # cloud_logger = logging.getLogger('cloudLogger')
@@ -46,54 +47,117 @@ cloud_logger.addHandler(handler)
 # file_path= '/Users/phonavitra/Desktop/term 5/Service Studio/Test/Others__Social Media__Cloud_function_test.csv'
 
 def process_social_media_data(file_path, product, source):
-    
+    file_path_str = str(file_path)
     try:
         # Load data from Excel or CSV
-        if file_path.lower().endswith('.xls') or file_path.lower().endswith('.xlsx'):
+        if file_path_str.lower().endswith('.xls') or file_path_str.lower().endswith('.xlsx'):
             data = pd.read_excel(file_path)
-            cloud_logger.info(f"Excel data loaded successfully from {file_path}.")
-        elif file_path.lower().endswith('.csv'):
+            print(f"Excel data loaded successfully from {file_path}.")
+        elif file_path_str.lower().endswith('.csv'):
             data = pd.read_csv(file_path)
-            cloud_logger.info(f"CSV data loaded successfully from {file_path}.")
+            print(f"CSV data loaded successfully from {file_path}.")
         else:
             raise ValueError(f"Unsupported file format for {file_path}. Only Excel (.xls, .xlsx) and CSV (.csv) files are supported.")
     except Exception as e:
-        publish_message(f"Error processing {file_path}: {e}")
-        cloud_logger.error(f"Error processing {file_path}: {e}")
+        publish_message(f"Error processing {file_path}: {e}","ERROR")
+        print(f"Error processing {file_path}: {e}")
         raise
 
     date_column = None
     for col in data.columns:
+        # if 'date' in col.lower() and data[col].apply(lambda x: isinstance(x, str) and '/' in x).all():
+        #     date_column = col
+        #     print(f"Columns in the DataFrame: {data.columns}")
+        #     print(f"Date column before formatting: {data[date_column]}")
+        #     data['formatted_date'] = data[date_column].apply(lambda x: format_date(x))
+        #     print(f"Date column after formatting: {data['formatted_date']}")
+        #     # data['formatted_date'] = data[col].apply(format_date)
+        #     # print("Date column after formatting:", data['formatted_date'])
+       
         if 'date' in col.lower() and data[col].apply(lambda x: isinstance(x, str) and '/' in x).all():
             date_column = col
-            data[col] = data[col].apply(format_date)
-            cloud_logger.info(f"Date column identified and formatted: {col}")
+            print(f"Date column identified: {col}")
+            print(f"Date column before formatting: {data[col]}")
+            data['formatted_date'] = data[date_column].apply(lambda x: format_date(x))
+            print(f"Date column after formatting: {data['formatted_date']}")
+            break
+            #data[col] = data[col].apply(format_date)
+           # print(f' data[col] is {data[col]}')
+            print(f"Date column identified and formatted: {col}")
+            break
+        elif 'date' in col.lower() and data[col].apply(lambda x: isinstance(x, str) and '-' in x).all():
+            date_column = col
+            print(f"Date column before formatting: {data[date_column]}")
+            data['formatted_date'] = data[date_column].apply(lambda x: format_date(x))
+            print(f"Date column after formatting: {data['formatted_date']}")
             break
     if date_column:
         data.rename(columns={date_column: 'Date'}, inplace=True)
 
     if not date_column:
-        publish_message("Error: No date column identified. Date column is required for processing")
-        cloud_logger.warning("No date column identified.")
+        publish_message("Error: No date column identified. Date column is required for processing",'ERROR')
+        print("No date column identified.")
+        raise ValueError("Date column is needed for processing")
 
     feedback_column = None
     for col in data.columns:
-        if 'feedback' in col.lower() or 'comments' in col.lower() or 'mentions' in col.lower():
-            feedback_column = col
-            cloud_logger.info(f"Feedback column identified: {col}")
+        if 'date' in col.lower() and data[col].apply(lambda x: isinstance(x, str) and '/' in x).all():
+            date_column = col
+            print(f"Date column before formatting: {data[date_column]}")
+            data[date_column] = data[date_column].apply(lambda x: format_date(x))  # Overwrite the original date column
+            print(f"Date column after formatting: {data[date_column]}")
             break
-
+        elif 'date' in col.lower() and data[col].apply(lambda x: isinstance(x, str) and '-' in x).all():
+            date_column = col
+            print(f"Date column before formatting: {data[date_column]}")
+            data[date_column] = data[date_column].apply(lambda x: format_date(x))  # Overwrite the original date column
+            print(f"Date column after formatting: {data[date_column]}")
+            break
+    print(f"All columns in the DataFrame: {data.columns}")
+    
+    for col in data.columns:
+        if 'feedback' in col.lower():
+            feedback_column = col
+            print(f"Feedback column identified: {col}")
+            break
+    # if feedback_column:
+    #     data = data[data[feedback_column].apply(lambda x: pd.notna(x) and is_english(x) and is_valid_feedback(x))]
+    #     data.rename(columns={feedback_column: 'Feedback'}, inplace=True)
     if feedback_column:
-        data = data[data[feedback_column].apply(lambda x: pd.notna(x) and is_english(x) and is_valid_feedback(x))]
+        print("Evaluating each row:")
+        for index, row in data.iterrows():
+            is_not_na = pd.notna(row[feedback_column])
+            is_eng = is_english(row[feedback_column])
+            is_valid = is_valid_feedback(row[feedback_column])
+            print(f"Row {index}: notna={is_not_na}, is_english={is_eng}, is_valid_feedback={is_valid}")
+
+        print(f"DataFrame before filtering:\n{data}")
+        filtered_data = data[data[feedback_column].apply(lambda x: pd.notna(x) and is_english(x) and is_valid_feedback(x))]
+        print(f"DataFrame after filtering:\n{filtered_data}")
+        print(f"Rows removed: {len(data) - len(filtered_data)}")
+        data = filtered_data
         data.rename(columns={feedback_column: 'Feedback'}, inplace=True)
     else:
-        publish_message("Error: No feedback column identified. Feedback column is required for processing")
-        cloud_logger.warning("No feedback column identified.")
+        print("No feedback column identified.")
         raise ValueError("Feedback column is required for processing.")
+    # else:
+    #     publish_message("Error: No feedback column identified. Feedback column is required for processing",'ERROR')
+    #     print("No feedback column identified.")
+    #     raise ValueError("Feedback column is required for processing.")
+    if not feedback_column:
+        print("No feedback column identified.")
+        raise ValueError("Feedback column is required for processing.")
+    if len(data) > 95:
+        error_message = f"Data exceeds the manageable row limit: {len(data)} rows. Keep upload dataset size to about 80-95 rows after transformation."
+        print(error_message)
+        publish_message(error_message, 'ERROR')
+        raise ValueError("Exceeded maximum row limit for processing.")
 
     if product != "Others":
-        data['Product'] = product
-        data['Subcategory'] = None
+        subproduct= find_best_match(product)
+        
+        data['Product'] = ''
+        data['Subcategory'] = subproduct
         data['Feedback Category'] = ''
         data['Sentiment'] = None
         data['Sentiment Score'] = None
@@ -122,9 +186,21 @@ def process_social_media_data(file_path, product, source):
     
 
     data = data.reindex(columns=desired_columns)
-    cloud_logger.info(f"Data received from social media processing: Type = {type(data)}, Shape = {data.shape if isinstance(data, pd.DataFrame) else 'N/A'}")
+    print(f"Data received from social media processing: Type = {type(data)}, Shape = {data.shape if isinstance(data, pd.DataFrame) else 'N/A'}")
 
 
     return data
 
+     #TODO: Add to SQL Analytics
+
+    # output_file_path = f'/path/to/output/Transformed_{product}_{source}_Social_Media.csv'
+    # try:
+    #     data.to_csv(output_file_path, index=False)
+    #     cloud_logger.info(f"Data transformation complete. File saved to: {output_file_path}")
+    # except Exception as e:
+    #     cloud_logger.error(f"Error saving transformed data: {e}")
+    #     raise
+
+# Example usage:
+# process_social_media_data('/Users/phonavitra/Desktop/term 5/Service Studio/raw part 1/Social Media mock data.csv', 'Product Name', 'Social Media Source')
 
